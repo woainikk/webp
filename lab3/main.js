5e9a0a6bbee12acafc151f4a79852c1f76d90033
woainikk <woainimrgrt@gmail.com>
2019-04-21 12:51:38 +0300

    lab3

--- lab3/main.js
+++ lab3/main.js
@@ -1,86 +1,96 @@
 const canvas = document.createElement('canvas');
 
-let path = 'https://source.unsplash.com/collection/1127180/';
-canvas.width = 800;
-canvas.height = 800;
+let path = 'https://source.unsplash.com/collection/190727/';
+canvas.width = 600;
+canvas.height = 400;
 const ctx = canvas.getContext('2d'),
 
     img1 = new Image(),
     img2 = new Image(),
-
-    xSize = getRandomInt(150, 450),
-    xResize = 300 - (xSize - 300),
-    xOffset = xSize - 300,
-    ySize = getRandomInt(100, 300),
-    yResize = 200 - (ySize - 200),
-    yOffset = ySize - 200,
     img3 = new Image(),
-    img4 = new Image();
+    img4 = new Image(),
 
+    xOffset = 50,
+    yOffset = 50;
+
+
+// чтобы можно было скачать картинку по том по правилам Cross-Origin Resource Sharing
 img1.crossOrigin = 'anonymous';
-img1.src = path + xSize + 'x' + ySize;
-
+img1.src = path + '350x250';
 
 const data = JSON.parse(getQuote());
 
 img1.onload = function () {
     ctx.drawImage(img1, 0, 0);
     img2.crossOrigin = 'anonymous';
-    img2.src = path + xSize + 'x' + yResize;
+    img2.src = path + '350x150';
+    //когда первая картинка готова, рисуем ее и идем за второй и тд
     img2.onload = function () {
         ctx.drawImage(img2, 0, 200 + yOffset);
         img3.crossOrigin = 'anonymous';
-        img3.src = path + xResize + 'x' + ySize;
+        img3.src = path + '250x250';
         img3.onload = function () {
             ctx.drawImage(img3, 300 + xOffset, 0);
             img4.crossOrigin = 'anonymous';
-            img4.src = path + xResize + 'x' + yResize;
+            img4.src = path + '250x150';
             img4.onload = function () {
                 ctx.drawImage(img4, 300 + xOffset, 200 + yOffset);
-                fillText()
+                fillText();
+                //прикрепляем созданную канву в документ
+                document.body.appendChild(canvas);
             };
         };
     };
 };
 
 
-document.body.appendChild(canvas);
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+function wrappedLines(ctx, text, maxWidth) {
+    const words = text.split(" ");
+    const lines = [];
+    let currentLine = words[0];
 
-function addText() {
-    let mas = data[0].quote.split(' ');
-    let countOfLines = Math.ceil(mas.length / 9);
-    ctx.textAlign = 'center';
-    for (var i = 0; i < countOfLines; i++) {
-        ctx.fillText(masToString(mas.slice(i * 9, (i + 1) * 9)), 300, 100 + 200 / (countOfLines + 1) * (i + 1))
+    for (let i = 1; i < words.length; i++) {
+        let word = words[i];
+        let width = ctx.measureText(currentLine + " " + word).width;
+        if (width < maxWidth) {
+            currentLine += " " + word;
+        } else {
+            lines.push(currentLine);
+            currentLine = word;
+        }
     }
-    ctx.textAlign = 'right';
-    ctx.fillText('(c) ' + data[0].character, 590, 380);
+    lines.push(currentLine);
+    return lines;
 }
 
 function fillText() {
-    ctx.font = 'Bold 18px Arial';
+    ctx.font = 'Bold 22px Arial';
     ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
     ctx.fillRect(0, 0, 600, 400);
-    ctx.fillStyle = "white";
-    addText()
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'top';
+    const fontSize = 22;
+    const textMargin = 5;
+
+
+    const textLines = wrappedLines(ctx, data[0].quote, 600 * 0.9);
+    const linesCount = textLines.length;
+    const textHeight = (linesCount * fontSize) + ((linesCount - 1) * textMargin);
+    const startY = (400 - textHeight) / 2;
+    textLines.forEach((line, i) => {
+        const lineY = startY + i * (fontSize + textMargin);
+        ctx.fillText(line, 300, lineY);
+    });
+    ctx.fillText('(c) ' + data[0].character, 450, 370);
 }
 
-function masToString(mas) {
-    var str = '';
-    for (let i = 0; i < mas.length; i++) {
-        str = str + mas[i] + ' '
-    }
-    return str
-}
-
+//вызывается по клику на канве
 canvas.onclick = function () {
-    var dataURL = canvas.toDataURL("image/jpeg");
-    var link = document.createElement("a");
-    document.body.appendChild(link); // Firefox requires the link to be in the body :(
+    let dataURL = canvas.toDataURL("image/jpeg");
+    let link = document.createElement("a");
+    document.body.appendChild(link);
     link.href = dataURL;
     link.download = "q.jpg";
     link.click();
@@ -88,19 +98,10 @@ canvas.onclick = function () {
 };
 
 function getQuote() {
-    let response = null;
+
     const xhr = new XMLHttpRequest();
-
     xhr.open('GET', 'https://thesimpsonsquoteapi.glitch.me/quotes', false);
-
     xhr.send();
-    if (xhr.status !== 200) {
-        alert(xhr.status + ': ' + xhr.statusText);
-    } else {
-        response = xhr.responseText;
-        console.log(response);
+    return xhr.responseText;
 
-    }
-
-    return response;
 }
